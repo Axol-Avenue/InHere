882f6bd3732379febac5e93261710ebead08977f
Gabriel Ferrer <goldferret1@gmail.com>
2024-03-19 18:43:06 -0500

    Testing Actions by changing colors of HomePage.css

--- InHere/src/pages/HomePage.jsx
+++ InHere/src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ function HomePage () {
             <h1>HomePage</h1>
             <span className="gridBackground">
                 <div className="pageContent">
-                    <div className="item item-1">Item 1</div>
+                    <div className="item item-1">Item TESTING SHIT</div>
                     <div className="item item-2">Item 2</div>
                     <div className="item item-3">Item 3</div>
                     <div className="item item-4">Item 4</div>
