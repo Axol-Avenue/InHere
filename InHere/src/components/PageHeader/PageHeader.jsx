fa181040a2ae27b886ae8a8008346de1b7b6395c
Gabriel Ferrer <goldferret1@gmail.com>
2024-03-22 14:06:23 -0500

    Set up Layout Component - Made Header Dynamic - Created grid for layout pages - Changed Header color to match SideNavBar - Removed App's flex display

--- InHere/src/components/PageHeader/PageHeader.jsx
+++ InHere/src/components/PageHeader/PageHeader.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import Sidebar from '../SideBar/Sidebar.jsx';
 import './PageHeader.module.css';
 
-const Header = () => {
+const Header = ({title}) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
     const toggleSidebar = () => {
@@ -12,7 +12,7 @@ const Header = () => {
 
     return (
         <div className={styles.header}>
-            <h1 className={styles.heading}>Calendar Page</h1>
+            <h1 className={styles.heading}>{title}</h1>
             <button className={styles.button} onClick={toggleSidebar}>Settings</button>
             {isSidebarOpen && <Sidebar />}
         </div>
