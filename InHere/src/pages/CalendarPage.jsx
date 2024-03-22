63533713220e60ff2e000063b342ba0500e380cc
KrisYG <kyahel@hotmail.com>
2024-03-22 11:46:13 -0500

    Header fixes

--- InHere/src/pages/CalendarPage.jsx
+++ InHere/src/pages/CalendarPage.jsx
@@ -1,8 +1,10 @@
 import Header from "../components/PageHeader/PageHeader.jsx";
+import styles from "../components/PageHeader/PageHeader.module.css";
+
 
 function CalendarPage () {
     return (
-        <div>
+        <div className={styles.layout}>
             <Header />
         </div>
     )
