34577b1cd2b092f346fb213c7ef6e2348bf660f7
Jacob Brockett <sexyjacob@dhcp-172-24-33-187.utsarr.net>
2024-03-28 23:39:13 -0500

    api attempt 3/28 11:39pm

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 const app = express();
 app.use( cors() );
-app.use(express.json);
+app.use(express.json());
 
 const db = mysql.createConnection({
     host: "localhost", // TODO: find host for database on web server!
@@ -13,21 +13,15 @@ const db = mysql.createConnection({
     database: "inhere"
 });
 
-// db.connect(err => {
-//     if(err){
-//         return err;
-//     }
-// })
+db.connect(err => {
+    if (err) {
+        console.error("Error connecting to MySQL database:", err);
+        process.exit(1);
+    }
+    console.log("MySQL database connected!");
+});
 
-// console.log(db)
-
-// app.options('*', cors())
 app.post('/signUp', (req, res) => {
-
-    // Testing if the connection to the database is working
-    // if(db.state === 'disconnected'){
-    //     console.log("Db connection is unavailable (disconnected)");
-    // }
 
     const sql = "INSERT INTO User (`Email`, `Username`, `Password`) VALUES (?,?,?)";
 
@@ -37,18 +31,16 @@ app.post('/signUp', (req, res) => {
         req.body.password
     ];
 
-    // Test query
-    db.query("INSERT INTO User (`Email`, `Username`, `Password`) VALUES ('test', 'test2', 'test3')")
 
-    db.query(sql, [values], (err, data) => {
+    db.query(sql, values, (err, data) => {
         if(err)
         {
-            return res.json("Error");
+            return res.json("Error inserting data into the database");
         }
         return res.json(data);
     })
 })
 
 app.listen(3307, () => {
-    console.log("listening");
+    console.log("listening on port 3307");
 })
