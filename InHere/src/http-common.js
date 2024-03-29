fa4cf28b2e39aff021b0d89d64e6d32559523ad4
Gabriel Ferrer <goldferret1@gmail.com>
2024-03-29 13:34:52 -0500

    Added a second header to http-common.js

--- InHere/src/http-common.js
+++ InHere/src/http-common.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
     baseURL: "http://localhost:3307/Backend",
     headers: {
+        "Access-Control-Allow-Origin": "*",
         "Content-type": "application/json"
     }
 });
