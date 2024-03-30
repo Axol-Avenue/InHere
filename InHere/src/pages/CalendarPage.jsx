8c04e0d58951467a3dcd59aa86bc23a6da1cbf9a
KrisYG <kyahel@hotmail.com>
2024-03-30 15:45:05 -0500

    Header fixes

--- InHere/src/pages/CalendarPage.jsx
+++ InHere/src/pages/CalendarPage.jsx
@@ -1,11 +1,11 @@
-import Header from "../components/PageHeader/PageHeader.jsx";
 import styles from "../components/PageHeader/PageHeader.module.css";
-
+import layout from "../pages/css-files/CalendarPage.module.css";
+import Calendar from "../components/Calendar/Calendar.jsx";
 
 function CalendarPage () {
     return (
-        <div className={styles.layout}>
-            {/*<Header />*/}
+        <div >
+            <Calendar />
         </div>
     )
 }
