49cddf046a8d8364adbe81799511a5a4d73bfbc1
Gabriel Ferrer <goldferret1@gmail.com>
2024-03-30 21:25:42 -0500

    URL Change in SignUpPage.jsx (x2)

--- InHere/src/pages/SignUpPage.jsx
+++ InHere/src/pages/SignUpPage.jsx
@@ -31,7 +31,7 @@ function SignUpPage () {
             };
 
             // Call API w/ Fetch:
-            const url = "http://ec2-18-223-107-62.us-east-2.compute.amazonaws.com:3307/signUp";
+            const url = "https://ec2-18-223-107-62.us-east-2.compute.amazonaws.com:3307/signUp";
             const options = {
                 method: "POST",
                 headers: {
