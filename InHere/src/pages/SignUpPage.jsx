0c9934ca9268d3a1e16e0ff5f7099ec9e9833452
Gabriel Ferrer <goldferret1@gmail.com>
2024-03-30 19:07:02 -0500

    Change reverted

--- InHere/src/pages/SignUpPage.jsx
+++ InHere/src/pages/SignUpPage.jsx
@@ -31,7 +31,7 @@ function SignUpPage () {
             };
 
             // Call API:
-            http.post('https://localhost:3307/signUp', values)
+            http.post('http://localhost:3307/signUp', values)
                 .then(res => {
 
                     console.log(res);
