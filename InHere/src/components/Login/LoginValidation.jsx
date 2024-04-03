0a7b7a6a8bd3f8fc6efdeacc42ed2c8c1d084532
Gabriel Ferrer <goldferret1@gmail.com>
2024-04-03 14:24:30 -0500

    Added Validation to LoginValidation.jsx - Similar to SignUpValidation.jsx

--- InHere/src/components/Login/LoginValidation.jsx
+++ InHere/src/components/Login/LoginValidation.jsx
@@ -1,21 +1,32 @@
 function LoginValidation (username, password){
     const errors = {};
 
-    if (username === '')
-    {
+    const validUsername = new RegExp(
+        '.{6,}'
+    );
+    const validPassword = new RegExp(
+        '^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$'
+    );
+
+    // Username validation checks
+    if (username === '') {
         errors.username = " Username should not be empty!";
     }
-    else
-    {
+    else if (!validUsername.test(username)) {
+        errors.username = " Invalid username, please make sure it is longer than five letters!"
+    }
+    else {
         errors.username = "";
     }
 
-    if (password === '')
-    {
+    // Password validation checks
+    if (password === '') {
         errors.password = " Password should not be empty!";
     }
-    else
-    {
+    else if (!validPassword.test(password)) {
+        errors.password = " Invalid Password, please make sure it is longer than five letters and has a number!"
+    }
+    else {
         errors.password = "";
     }
 
