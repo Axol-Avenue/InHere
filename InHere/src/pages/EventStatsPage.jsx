d274d7db8dc1be7b42f1e43f82a2dac75b98a1ef
Jacob Brockett <sexyjacob@dhcp-172-24-33-187.utsarr.net>
2024-04-12 17:47:06 -0500

    fix api call to event stats page attempt 5

--- InHere/src/pages/EventStatsPage.jsx
+++ InHere/src/pages/EventStatsPage.jsx
@@ -38,7 +38,7 @@ function EventStatsPage () {
 
                 if(res.data.message === 'Query Successful')
                 {
-                    setEventStats(res.data.results);
+                    setEventStats(res.data);
                 }
                 else
                 {
