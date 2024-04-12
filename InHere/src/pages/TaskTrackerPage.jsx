001780b223a09f14375b90423603a0fd844cb463
Gabriel Ferrer <goldferret1@gmail.com>
2024-04-12 10:55:49 -0500

    Added functionality to get events only within the displayed week - Moved axios post to its own function

--- InHere/src/pages/TaskTrackerPage.jsx
+++ InHere/src/pages/TaskTrackerPage.jsx
@@ -2,9 +2,14 @@ import styles from "../components/PageHeader/PageHeader.module.css";
 import Calendar from "../components/Calendar/ListCalendar.jsx";
 
 function TaskTrackerPage () {
+
+    var cal = new Calendar();
+    //console.log(cal.view.currentStart);
+
+
     return (
         <div className={styles.layout}>
-            <Calendar/>
+            {cal}
         </div>
     )
 }
