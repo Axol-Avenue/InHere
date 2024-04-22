5b171b96912db98235681dc1b2173a8370086454
KrisYG <kyahel@hotmail.com>
2024-04-22 18:33:26 -0500

    AddEvent will finally work

--- Backend/server.js
+++ Backend/server.js
@@ -232,18 +232,24 @@ app.post('/readEvents', (req, res) => {
 
 // Add new event to calendar
 app.post('/addEvent', (req, res) => {
-    const events = req.body.events;
-    const userID = req.body.userID;
+
+    const values = [
+        req.body.events,
+        req.body.userID
+    ]
 
     const sql = "UPDATE Calendar SET 'Data' = ? WHERE 'UserID' = ?";
 
     // Query to update database with updated json of events
-    db.query(sql, [events, userID], (err, result) => {
+    db.query(sql, values, (err, result) => {
         if(err) {
             console.error("Error updating events in the database:", err);
             return res.status(500).json({ error: "Error updating events in the database" });
         }
-        return res.status(200).json({ message: "Events updated successfully" });
+        return res.status(200).json({
+            message: "Events updated successfully",
+            result: result
+        });
     })
 })
 
